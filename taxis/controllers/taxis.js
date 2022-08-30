ea932f53cd568072849ebad06c5e10b8942495cb
ahazard <jane15q@aol.com>
2022-08-30 14:30:11 -0400

    add routes to taxis, controllers to taxis

--- taxis/controllers/taxis.js
+++ taxis/controllers/taxis.js
@@ -8,6 +8,20 @@ function index(req, res) {
     });
   };
 
+  function details(req, res) {
+    Taxi.findById(req.params.id, function(err, taxi) {
+        Ticket.find({taxi: taxi._id}, function(err, reviews){
+            res.render('flights/details', {title: 'Taxi Detail', taxi, reviews});
+        });
+    });
+};
+
+function newTrip(req, res) {
+  res.render("taxis/new");
+};
+
 module.exports = {
     index,
+    details,
+    new: newTrip,
 }
--- taxis/routes/taxis.js
+++ taxis/routes/taxis.js
@@ -6,11 +6,11 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Taxis' });
 });
 
-router.get('/', flightsCtrl.index);
-router.get("/new", flightsCtrl.new)
-router.post("/new", flightsCtrl.create);
-router.get("/:id", flightsCtrl.show);
-router.delete("/:id", flightsCtrl.deleteFlight);
+router.get("/", taxisCtrl.index);
+router.get("/new", taxisCtrl.newTrip);
+router.get("/:taxiId", taxisCtrl.detail);
+router.post(":id/review", taxisCtrl.createReview);
+router.put("/:taxiId/review/:reviewId". taxisCtrl.editReview);
+router.delete("/:taxiId/review/:reviewId", taxisCtrl.deleteReview);
 
-module.exports = router;
 module.exports = router;
